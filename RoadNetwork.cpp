e9206e7d4ab88eb436404af9538979a342fcfde1
dyunus21 <diyayunus@outlook.com>
2022-04-12 21:30:59 -0500

    Style fixes

--- RoadNetwork.cpp
+++ RoadNetwork.cpp
@@ -6,11 +6,13 @@ RoadNetwork::RoadNetwork() {
     std::vector<Edge> edges;
     std::vector<Node> nodes;
 
+    // Created an empty adjacency matrix of size nodes x nodes
     for(int i = 0; i<nodes.size();i++) {
         std::vector<double> row(nodes.size());
         adjacencyMatrix.push_back(row);
     }
 
+    // Stores L2 Distances between every node
     for (Edge edge : edges) {
         adjacencyMatrix[edge.startNodeID][edge.endNodeId] = edge.distance;
         adjacencyMatrix[edge.endNodeId][edge.startNodeID] = edge.distance;
