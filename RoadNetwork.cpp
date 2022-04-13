110d779fb74b4c0897a5b18f0fccf7ab6f1f9601
dyunus21 <diyayunus@outlook.com>
2022-04-12 21:23:28 -0500

    Added adjacency matrix implementation in RoadNetwork constructor

--- RoadNetwork.cpp
+++ RoadNetwork.cpp
@@ -1,3 +1,19 @@
 #include "RoadNetwork.h"
 
-std::vector<std::vector<float>> createAdjacencyMatrix()
+RoadNetwork::RoadNetwork() {
+    // @ Kriti add IMPORT
+
+    std::vector<Edge> edges;
+    std::vector<Node> nodes;
+
+    for(int i = 0; i<nodes.size();i++) {
+        std::vector<double> row(nodes.size());
+        adjacencyMatrix.push_back(row);
+    }
+
+    for (Edge edge : edges) {
+        adjacencyMatrix[edge.startNodeID][edge.endNodeId] = edge.distance;
+        adjacencyMatrix[edge.endNodeId][edge.startNodeID] = edge.distance;
+    }
+
+}
