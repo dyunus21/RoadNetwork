4b3778e05363f5afb22c68fbdc75c86f7dcd17c7
dyunus21 <diyayunus@outlook.com>
2022-04-18 23:00:59 -0500

    Modified driver file

--- src/driver.cc
+++ src/driver.cc
@@ -3,12 +3,13 @@
 #include "utilities.hpp"
 
 int main() {
-    // roadNetwork network;
-    // dijkstras dijkstra;
-    // int nodeId = 0;
-    // std::vector<std::vector<double> > graph = network.getAdjacencyMatrix();
+    roadNetwork network;
+    dijkstras dijkstra;
+    int nodeId;
+    std::vector<std::vector<double> > graph = network.getAdjacencyMatrix();
     std::cout << "Hello, please enter a node Id!" << std::endl;
-    // std::cin >> nodeId;
+    std::cin >> nodeId;
+    std::cout<<nodeId<<std::endl;
     // std::vector<float> dijkstraGraph = dijkstra.dijkstra_path(graph, nodeId );
-
+    return 0;
 }
